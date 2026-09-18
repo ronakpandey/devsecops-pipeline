f380eeb78551953cf638c6cacfe67a1a2f73ac36
Ronak Pandey <pandeyronak2004@gmail.com>
2026-09-18 19:24:01 +0530

    Fix case-sensitivity and add test secret

--- App/app.py
+++ App/app.py
@@ -12,3 +12,20 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# SIMULATED SECRET LEAK (TO TEST DEVSECOPS PIPELINE FAILURE)
+AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLEAKIAIOSFODNN7EXAMPLE"
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "online",
+        "message": "DevSecOps Production Service Running",
+        "version": "1.0.0"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)    
