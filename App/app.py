e27bcaadd837a4f85d693218e304ca407e1a85cb
Ronak Pandey <pandeyronak2004@gmail.com>
2026-09-18 19:25:49 +0530

    Clean up app.py with simulated secret key

--- App/app.py
+++ App/app.py
@@ -2,20 +2,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return jsonify({
-        "status": "online",
-        "message": "DevSecOps Production Service Running",
-        "version": "1.0.0"
-    })
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
 # SIMULATED SECRET LEAK (TO TEST DEVSECOPS PIPELINE FAILURE)
 AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLEAKIAIOSFODNN7EXAMPLE"
 
@@ -28,4 +14,4 @@ def home():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)    
+    app.run(host="0.0.0.0", port=5000)
